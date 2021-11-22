258657f3330ffa924ff6a7721c3d2de6dacd7ab8
Владимир <lordamncer1998electro@yandex.ru>
2021-11-22 19:54:36 +0400

    global styles are declared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarPanelComponent } from './navbar-panel/navbar-panel.component';
+import { BasketComponent } from './basket/basket.component';
+import { LoginComponent } from './login/login.component';
+import { SearchPanelComponent } from './search-panel/search-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarPanelComponent,
+    BasketComponent,
+    LoginComponent,
+    SearchPanelComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/navbar-panel/navbar-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-panel',
+  templateUrl: './navbar-panel.component.html',
+  styleUrls: ['./navbar-panel.component.css']
+})
+export class NavbarPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
