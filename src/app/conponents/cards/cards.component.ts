225a5672f033269b1687fcaaf789d2213a8d260c
Владимир <lordamncer1998electro@yandex.ru>
2021-11-23 00:27:17 +0400

    Added product cards

---
+++ src/app/conponents/cards/cards.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Product} from "../../models/product.model";
+import {products} from "../../mock/data.mock";
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+export class CardsComponent implements OnInit {
+
+  public data: Product[];
+
+  constructor() {
+    this.data = products
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
