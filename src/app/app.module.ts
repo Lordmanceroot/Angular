3e7ff259b1514442745f8075527e7b8f58067ffe
Владимир <lordamncer1998electro@yandex.ru>
2021-11-23 00:37:28 +0400

    small renaming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './conponents/header/header.component';
-import { NavbarPanelComponent } from './conponents/navbar-panel/navbar-panel.component';
-import { BasketComponent } from './conponents/basket/basket.component';
-import { LoginComponent } from './conponents/login/login.component';
-import { CardsComponent } from './conponents/cards/cards.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarPanelComponent } from './components/navbar-panel/navbar-panel.component';
+import { BasketComponent } from './components/basket/basket.component';
+import { LoginComponent } from './components/login/login.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
