686a2c8c291585d5681db85aad329ba3a9f3d322
Владимир <lordamncer1998electro@yandex.ru>
2022-01-14 20:16:33 +0400

    made some changes

--- src/app/mock/data.mock.ts
+++ src/app/mock/data.mock.ts
@@ -3,26 +3,26 @@ import { Product } from "../models/product.model";
 export const products: Product[] = [
   {
     id: 1,
-    image: "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
-    name: "1Смартфон POCO F3 (8/256gb) черный/ Night Black",
+    image: "https://www.wishbytech.com/wp-content/uploads/2021/06/SMARTPHONE-SAMSUNG-A52-SM-A525DS-AWSOME-BLACK-3-600x600.png",
+    name: "Samsung A52 (8/256gb) черный/ Night Black",
     price: 31990
   },
   {
     id: 2,
     image: "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
-    name: "2Смартфон POCO F3 (8/256gb) черный/ Night Black",
+    name: "Xiaomi POCO F3 (8/256gb) черный/ Night Black",
     price: 31990
   },
   {
     id: 3,
-    image: "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
-    name: "3Смартфон POCO F3 (8/256gb) черный/ Night Black",
+    image: "https://specs-tech.com/wp-content/uploads/2020/03/Huawei-P40-Pro-Premium.jpg",
+    name: "Huawei P40 PRO (8/256gb) черный/ Night Black",
     price: 31990
   },
   {
     id: 4,
-    image: "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
-    name: "4Смартфон POCO F3 (8/256gb) черный/ Night Black",
+    image: "https://www.ixbt.com/img/n1/news/2020/5/1/gsmarena_002.jpg",
+    name: "Vivo v19 (8/256gb) черный/ Night Black",
     price: 31990
   }
 ]
