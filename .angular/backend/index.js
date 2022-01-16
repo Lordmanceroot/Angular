fc6898bd0ed8ebbe4a521bc49dd375a9a976dac2
Владимир <lordamncer1998electro@yandex.ru>
2022-01-16 15:38:59 +0400

    test

---
+++ .angular/backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const {products} = require("../../src/app/mock/data.mock");
+const app = express()
+const port = 3000
+
+app.get('/products', (req, res) => {
+  res.send(products);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -9,7 +9,7 @@ import {QuoteService} from "../../services/quote.service";
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-  data: any
+  data: any;
 
   constructor(private activatedRoute: ActivatedRoute,
               private cardService: CardService,
@@ -17,14 +17,12 @@ export class ProductDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.params['id']
-    console.log(id)
     if (id) {
-      this.data = this.cardService.getProduct(+id)
+      this.data = this.cardService.getProduct(+id);
     }
-    console.log(this.data)
   }
 
-  addQuote(id: number) {
+  addCardInBasket(id: number) {
     this.quoteService.addCardInBasket(id);
   }
 }
